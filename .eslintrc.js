7aede3db956a9a8b2ba0591675a53336c7f6856d
Jean Kenned <jeankennedteodoro@gmail.com>
2021-12-18 09:23:25 -0300

    chore: config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "standard-with-typescript",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig-eslint.json"
+      },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "ecmaFeatures": {
+        "arrowFunctions":true,
+      },
+    "rules": {
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/comma-spacing": "off",
+        "@typescript-eslint/return-await": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "no-redeclare": "off",
+        "import/export": "off",
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error"
+    }
 };
