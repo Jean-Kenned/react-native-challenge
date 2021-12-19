c1aa5a9268491eccf5f4eb4fb9e6fdd42641e716
Jean Kenned <jeankennedteodoro@gmail.com>
2021-12-19 14:37:18 -0300

    feat: export created components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,2 +1,5 @@
 export { default as ShoppingBag } from './ShoppingBag'
 export { default as SearchInput } from './SearchInput'
+export { default as ProductCard } from './ProductCard'
+export { default as DiscountLabel } from './DiscountLabel'
+export { default as PriceMember } from './PriceMember'
