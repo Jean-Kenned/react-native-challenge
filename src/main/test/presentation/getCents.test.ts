c0d807f7a15fc175f58d6a2dac8ca1caea5e502d
Jean Kenned <jeankennedteodoro@gmail.com>
2021-12-19 13:46:25 -0300

    test: add getCents test

---
+++ src/main/test/presentation/getCents.test.ts
@@ -0,0 +1,7 @@
+import {getCents} from '@/presentation/components/PriceMember/getCents'
+
+describe('Validate getCents function', () => {
+  test('Should return the correct cents value formatted', () => {
+    expect(getCents(30.4)).toBe('40')
+  })
+})
